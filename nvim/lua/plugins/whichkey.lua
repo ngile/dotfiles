6c4ed643b6548785c9405bac5e79a59f000213f7
Georgi Kostadinov <georgi.kostadinov@softwaregroup.com>
2024-03-04 22:38:14 +0200

    Add harpoon plugin

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -45,7 +45,7 @@ return {
 					b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
 					c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
 					f = { "<cmd>Telescope find_files<cr>", "Find files" },
-					p = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
+					p = { "<cmd>lua require('telescope').extensions.project.project()<cr>", "Projects" },
 					t = { "<cmd>Telescope live_grep<cr>", "Find Text" },
 					s = { "<cmd>Telescope grep_string<cr>", "Find String" },
 					h = { "<cmd>Telescope help_tags<cr>", "Help" },
