a6162aa1ddf3b4eabba8647742af811518fbf8b7
Jenkins <jenkins@backseatgaming.com>
2024-11-11 00:54:20 +0200

    Update .zshrc

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,12 +1,12 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-config.font = wezterm.font("Cousine Nerd Font")
-config.font_size = 12
-
-config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
--- config.color_scheme_dirs = { "./colors/melange_dark.toml" }
-config.color_scheme = "melange_dark"
-config.window_background_opacity = 0.8
+config = {
+	font = wezterm.font("Cousine Nerd Font"),
+	font_size = 12,
+	enable_tab_bar = false,
+	window_decorations = "RESIZE",
+	color_scheme = "melange_dark",
+	window_background_opacity = 0.9,
+}
 
 return config
