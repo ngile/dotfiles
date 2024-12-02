18319aa8fd08c8858fe864da0a9b62ef13a9a40d
Jenkins <jenkins@backseatgaming.com>
2024-12-02 23:24:59 +0200

    Add gitconfig

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,6 +7,17 @@ config = {
 	window_decorations = "RESIZE",
 	color_scheme = "melange_dark",
 	window_background_opacity = 0.9,
+	max_fps = 120,
+	default_cursor_style = "SteadyBlock",
+	cursor_blink_ease_in = "Constant",
+	-- Setting this to 0 disables blinking
+	cursor_blink_rate = 0,
+	window_padding = {
+		left = 2,
+		right = 2,
+		top = 15,
+		bottom = 0,
+	},
 }
 
 return config
